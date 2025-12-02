684751e39e0fad6902f00f9226fa68f749723d97
Johan Arango <johan.arango@sasoftco.com.co>
2025-12-02 15:56:22 -0500

    port changed

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from app import create_app
 
-# Crea la aplicación usando la fábrica que definimos en app/__init__.py
+
 app = create_app()
 
 if __name__ == '__main__':
-    print("--- 🚦 INICIANDO SERVIDOR TRAFFIC AI ---")
-    print(" > Abre tu navegador en: http://127.0.0.1:5000")
-    print(" > Presiona CTRL+C en esta terminal para detenerlo.")
-    
-    # Iniciamos el servidor
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=8080, debug=False)
